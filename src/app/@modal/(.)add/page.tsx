f629916b0a611ce272a3eb70adc9e725798dafd5
Miklos Daniel <daniel.miklos0@gmail.com>
2023-10-09 11:09:08 +0200

    add intercepted add page

--- src/app/@modal/(.)add/page.tsx
+++ src/app/@modal/(.)add/page.tsx
@@ -2,9 +2,6 @@ import EditPanel from "@/components/EditPanel";
 import Modal from "@/components/Modal";
 
 export default function InterceptedAddPage() {
-  async function createContact(formData: FormData) {
-    "use server";
-  }
   return (
     <Modal>
       <EditPanel mode="add" />
