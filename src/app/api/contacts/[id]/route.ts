a9dbcee75dd68e0c4bcc89be244b3bf49f04f2d9
Miklos Daniel <daniel.miklos0@gmail.com>
2023-10-09 11:08:26 +0200

    implement server api

---
+++ src/app/api/contacts/[id]/route.ts
@@ -0,0 +1,99 @@
+import prisma from "@/lib/prisma";
+import { contactSchema } from "@/lib/schemas";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest, { params }: any) {
+  const id = Number(params.id);
+  const contact = await prisma.contact.findMany({
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      phone: true,
+      Image: {
+        select: {
+          id: true,
+        },
+      },
+    },
+    where: {
+      id,
+      isDeleted: false,
+    },
+  });
+
+  if (!contact) {
+    return NextResponse.error();
+  }
+
+  return NextResponse.json({
+    ...contact,
+  });
+}
+
+export async function PUT(request: any) {
+  const body = await request.json();
+
+  const schemaResult = contactSchema.safeParse(body);
+  if (!schemaResult.success) {
+    console.error(schemaResult.error);
+    return NextResponse.error();
+  }
+
+  const { name, email, phone, image } = schemaResult.data;
+
+  const contact = await prisma.contact.create({
+    data: {
+      name,
+      email,
+      phone,
+    },
+  });
+
+  if (!contact) {
+    return NextResponse.error();
+  }
+
+  let dbImage = null;
+  if (image) {
+    dbImage = await prisma.image.create({
+      data: {
+        name: `${name}_${new Date().getTime()}`,
+        content: Buffer.from(image.split(",")[1], "base64"),
+        contactId: contact.id,
+      },
+    });
+  }
+
+  return NextResponse.json({
+    ...contact,
+    Image: dbImage,
+  });
+}
+
+export async function DELETE(request: NextRequest, { params }: any) {
+  const id = Number(params.id);
+  const contact = await prisma.contact.findFirst({
+    where: {
+      id,
+    },
+  });
+
+  if (!contact) {
+    return NextResponse.error();
+  }
+
+  const res = await prisma.contact.delete({
+    where: {
+      id,
+    },
+  });
+
+  if (!res) {
+    return NextResponse.error();
+  }
+
+  return NextResponse.json({
+    ...contact,
+  });
+}
---
+++ src/app/api/contacts/route.ts
@@ -0,0 +1,70 @@
+import prisma from "@/lib/prisma";
+import { contactSchema } from "@/lib/schemas";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function getContacts() {
+  return await prisma.contact.findMany({
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      phone: true,
+      Image: {
+        select: {
+          id: true,
+        },
+      },
+    },
+    where: {
+      isDeleted: false,
+    },
+  });
+}
+
+export async function GET(request: any) {
+  const contacts = await getContacts();
+  return NextResponse.json(contacts);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  const schemaResult = contactSchema.safeParse(body);
+  if (!schemaResult.success) {
+    console.error(schemaResult.error);
+    return NextResponse.error();
+  }
+
+  const { name, email, phone, image } = schemaResult.data;
+
+  const contact = await prisma.contact.create({
+    data: {
+      name,
+      email,
+      phone,
+    },
+  });
+
+  if (!contact) {
+    return NextResponse.error();
+  }
+
+  let dbImage = null;
+  if (image) {
+    dbImage = await prisma.image.create({
+      data: {
+        name: `${name}_${new Date().getTime()}`,
+        content: Buffer.from(image.split(",")[1], "base64"),
+        contactId: contact.id,
+      },
+    });
+  }
+
+  if (!contact) {
+    return NextResponse.error();
+  }
+
+  return NextResponse.json({
+    contact,
+  });
+}
