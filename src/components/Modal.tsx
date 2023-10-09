398e603f0c0576b3c9dee7bf8c9100a9a2bab79e
Miklos Daniel <daniel.miklos0@gmail.com>
2023-10-09 11:24:06 +0200

    add basic init animation

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,21 +1,25 @@
 "use client";
 import { useRef } from "react";
 import { useRouter } from "next/navigation";
+import { motion } from "framer-motion";
 
 export default function Modal(props: { children: React.ReactNode }) {
   const overlay = useRef(null);
   const router = useRouter();
 
   return (
-    <div
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.3 }}
       ref={overlay}
       onClick={(e) => e.target === overlay.current && router.back()}
       className="fixed inset-0 bg-black-100/60 z-50 "
       id="show-modal"
     >
-      <div className="bg-grey-100 absolute left-1/2 top-1/2  min-w-[404px] max-w-3xl -translate-x-1/2 -translate-y-1/2 rounded-sm">
+      <motion.div className="bg-grey-100 absolute left-1/2 top-1/2  min-w-[404px] max-w-3xl -translate-x-1/2 -translate-y-1/2 rounded-sm">
         {props.children}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 }
